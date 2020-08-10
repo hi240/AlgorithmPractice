00170db8901617f1a158cbb1dbc9b75a7367b00b
240__ <240__@192.168.219.176>
2020-08-10 23:47:05 +0900

    백준2562-8/10

--- src/Arrangement2562.java
+++ src/Arrangement2562.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 첫 째 줄부터 아홉 번째 줄까지 한 줄에 하나의 자연수가 주어진다. 주어지는 자연수는 100 보다 작다.
 
 출력
-첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다.
+첫째 줄에 최댓값을 출력하고, 둘째 줄에 최댓값이 몇 번째 수인지를 출력한다..
  */
 public class Arrangement2562 {
 	public static void main(String[] args) {
