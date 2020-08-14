37a4608684bbb7089236e5ab2e4280799c65dd52
240__ <240__@DESKTOP-3CBK7NI>
2020-08-14 21:51:18 +0900

    백준8958-08/14

---
+++ src/Arrangement8958.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/*
+"OOXXOXXOOO"와 같은 OX퀴즈의 결과가 있다. O는 문제를 맞은 것이고, X는 문제를 틀린 것이다. 문제를 맞은 경우 그 문제의 점수는 그 문제까지 연속된 O의 개수가 된다.
+예를 들어, 10번 문제의 점수는 3이 된다.
+
+"OOXXOXXOOO"의 점수는 1+2+0+0+1+0+0+1+2+3 = 10점이다.
+
+OX퀴즈의 결과가 주어졌을 때, 점수를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 테스트 케이스의 개수가 주어진다. 각 테스트 케이스는 한 줄로 이루어져 있고, 길이가 0보다 크고 80보다 작은 문자열이 주어진다. 문자열은 O와 X만으로 이루어져 있다.
+ */
+public class Arrangement8958 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a= sc.nextInt();
+		String[] array = new String[a];
+		for(int i=0;i<a;i++) {
+			array[i]=sc.next();
+			int sum=0;
+			int count=0;
+			for(int j=0;j<array[i].length();j++) {
+				if(array[i].charAt(j)=='O') {
+					sum += ++count;
+				}else {
+					count=0;
+				}
+			}
+			System.out.println(sum);
+		}
+		sc.close();
+	}
+}
+
