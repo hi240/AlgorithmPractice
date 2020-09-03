2102490aa53bdb884c553d862321a1d6ba88f2f3
240__ <240__@naver.com>
2020-09-03 14:22:24 +0900

    Programmers5-9/3

--- src/Programmers5.java
+++ src/Programmers5.java
@@ -1,6 +1,28 @@
-class Solution {
+/*
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+재한사항
+s는 길이가 1 이상, 100이하인 스트링입니다.
+입출력 예
+s	return
+abcde	c
+qwer	we
+*/
+
+
+class Programmers5 {
     public String solution(String s) {
         String answer = "";
+        
+        if(s.length()/2==0) {
+        	int a= (s.length()/2); 
+        	answer =s.substring(a-1,a+1); 
+        }else {
+        	int a= (s.length()/2)+1; 
+        	answer =s.substring(a-1,a); 
+        }
+        
         return answer;
     }
 }
+
