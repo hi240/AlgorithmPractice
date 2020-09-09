bbb41c54d1ab2739a2322f7f605100d7b510aa46
240__ <240__@naver.com>
2020-09-09 15:42:22 +0900

    Programmers서울에서 김서방찾기-9/9

---
+++ src/Programmers9.java
@@ -0,0 +1,13 @@
+class Programmers9 {
+    public String solution(String[] seoul) {
+        int num=0;
+        for(int i=0;i<seoul.length;i++) {
+        	if(seoul[i].equals("Kim")) {
+        		num=i;
+        	}
+        }
+        String answer="김서방은 "+num+"에 있다";
+        return answer;
+    }
+}
+//딱 5분걸린 기초 문제
