55527d28e9014b614cb319d8157ccb3b643722fa
240__ <240__@naver.com>
2020-10-30 23:47:33 +0900

    Programmers19자연수거꾸로-10/30

--- src/Programmers1/Programmers19.java
+++ src/Programmers1/Programmers19.java
@@ -1,27 +1,27 @@
 package Programmers1;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Queue;
-
+import java.util.ArrayList;
+//자연수 거꾸로
 public class Programmers19 {
 
 	public static void main(String[] args) throws IOException{
 		
-		int a = 1234;
-		int[] answer = {};
-		int i=0;
-		while(a>0) {
-			answer[i]= a%10;
-			a /=10;
-			i++;
-		}
-		String[] s= {"d","s"};
-		
-		for(int i=0;i<s.length;i++) {
-			s[s.length-i] = s[i];
-		}
+		long n= 1234;
+	
+		        Integer k = (int)(long) n;
+		        
+		        ArrayList<Integer> arr = new ArrayList<>();
+		        while(k>0){
+		            arr.add(k%10);
+		            k /= 10;
+		        }
+		         int[] answer =new int [arr.size()];
+		        for(int i=0;i<answer.length;i++){
+		            answer[i]=arr.get(i);
+		        }
+		        System.out.println(answer[0]);
+		    }
 		
 	}
-}
+
