48ef20b74a05d9bea1ee40542e33edb7d5dba933
240__ <240__@naver.com>
2020-11-02 22:33:00 +0900

    Programmers19자연수 거꾸로-11/2

--- src/Programmers1/Programmers19.java
+++ src/Programmers1/Programmers19.java
@@ -2,26 +2,23 @@ package Programmers1;
 
 import java.io.IOException;
 import java.util.ArrayList;
-//자연수 거꾸로
+//자연수 거꾸로 long casting 하는데에 시간 많이 걸렸음..
 public class Programmers19 {
 
-	public static void main(String[] args) throws IOException{
-		
-		long n= 1234;
-	
-		        Integer k = (int)(long) n;
-		        
+class Solution {
+    public int[] solution(long n) {
 		        ArrayList<Integer> arr = new ArrayList<>();
-		        while(k>0){
-		            arr.add(k%10);
-		            k /= 10;
+		        while(n>0){
+		            arr.add((int)(n%10));
+		            n = n / 10;
 		        }
-		         int[] answer =new int [arr.size()];
+		        int[] answer =new int [arr.size()];
 		        for(int i=0;i<answer.length;i++){
 		            answer[i]=arr.get(i);
 		        }
-		        System.out.println(answer[0]);
-		    }
-		
+        return answer;
+    }
+}
+
 	}
 
