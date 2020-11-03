126775ff72c1f37f9c09420ab6cb05e0a4323209
240__ <240__@naver.com>
2020-11-03 23:45:08 +0900

    Programmers16 3진법 뒤집기 - 이전에 한거

--- src/Programmers1/Programmers16.java
+++ src/Programmers1/Programmers16.java
@@ -1,28 +1,25 @@
 package Programmers1;
 
 import java.io.IOException;
-
+//3진법 뒤집기
 public class Programmers16 {
 	public static void main(String[] args) throws IOException{
 		
 		int n= 125;
-		String temp ="";
-		
-		while(n>0) {
-			temp += String.valueOf(n%3);
-			n /= 3;
-		}
-		
-		n=Integer.parseInt(temp);
-		int num=0;
-		int m=0;
-		
-		for(int i=0;i<temp.length();i++) {
-			m = n%10;
-			num += Math.pow(3, i)*m;
-			n /=10;
-		}
-		
+		  String temp ="";
+	        while(n!=0) {
+				temp += String.valueOf(n%3);
+				n /= 3;
+			}
+			n=Integer.parseInt(temp);
+	        int length=(int)(Math.log10(n)+1);
+			int num=0;
+	        int m=0;
+			for(int i=0;i<length;i++) {
+				m = n%10;
+				num += Math.pow(3, i)*m;
+				n /=10;
+			}
 		System.out.println(num);
 	}
 }
