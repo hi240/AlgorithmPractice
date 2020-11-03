deb67e30de1684122949d8be88117a18185a51b0
240__ <240__@naver.com>
2020-11-03 23:27:12 +0900

    Programmers18이상한 문자 만들기(한번더)-11/3

--- src/Programmers1/Programmers18.java
+++ src/Programmers1/Programmers18.java
@@ -7,18 +7,21 @@ public class Programmers18 {
 
 	public static void main(String[] args) throws IOException{
 	
-			String s="try one";
+			String s="try hello world";
 			
 		   String[] answer =  s.split("");
-		   
+		   int j=0;
 	       for(int i=0;i<answer.length;i++){
-	           if(i%2==0){
+	    	  
+	    	   if(answer[i].equals(" ")) j=-1;
+	           if(j%2==0){
 	               answer[i]=answer[i].toUpperCase();
-	               System.out.println("i is "+i);
-	           }else if(i%2==1){
+	               System.out.println("i is "+j);
+	           }else if(j%2==1){
 	              answer[i]= answer[i].toLowerCase();
-	              System.out.println("i2 is "+i);
+	              System.out.println("i2 is "+j);
 	           }
+	           j++;
 	       }
 	       s="";
 	        for(String n:answer){
