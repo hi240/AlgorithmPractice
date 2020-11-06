735d8e43f1c9a936e195cd7de93ef5329822973e
240__ <240__@naver.com>
2020-11-06 17:07:13 +0900

    Programmers24실패율-11/6 (진행중)

---
+++ src/Programmers1/Programmers24.java
@@ -0,0 +1,26 @@
+package Programmers1;
+
+import java.io.IOException;
+
+public class Programmers24 {
+	public static void main(String[] args) throws IOException{
+		int[]stages = {1,2,3};
+		System.out.println(solution(3, stages));
+	}
+	
+    public static int[] solution(int N, int[] stages) {
+    	        int[] answer = {};
+    	        double[] fail = {2.3};
+    	        Arrays.sort(fail,new Comparator<double>() {
+    	        	@Override
+    	        	public int compare(double a,double b) {
+    	        		return a;
+    	        	}
+    	        });
+    	        return answer;
+    }
+}
+
+
+
+
