3d4c9172a1ec8abfb7b7a65fba31f356c109e430
240__ <240__@naver.com>
2020-11-09 23:25:33 +0900

    Programmers24 계속하는중..11/9

--- src/Programmers1/Programmers24.java
+++ src/Programmers1/Programmers24.java
@@ -1,25 +1,37 @@
 package Programmers1;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Programmers24 {
 	public static void main(String[] args) throws IOException{
 		int[]stages = {1,2,3};
-		System.out.println(solution(3, stages));
+		System.out.println(solution(3, stages)); 
+    	        
 	}
 	
     public static int[] solution(int N, int[] stages) {
     	        int[] answer = {};
     	        double[] fail = {2.3};
-    	        Arrays.sort(fail,new Comparator<double>() {
+    	        int[] arr = new int [fail.length];
+    	        for(int i=0;i<fail.length;i++) {
+    	        	fail[i] *=1000;
+    	        	fail[i]=Math.round(fail[i]);
+    	        	arr[i]= Integer.parseInt(String.valueOf(fail[i]));
+    	        }
+    	        //[125, 420, 500, 500, 0]
+    	        Arrays.sort(arr,new Comparator<Integer>() {
     	        	@Override 
     	        	public int compare(double a,double b) {
     	        		return a;
     	        	}
-    	        });
+    	        }); 
     	        return answer;
+    	        
+    	      
     }
 }
+ 
 
 
 
