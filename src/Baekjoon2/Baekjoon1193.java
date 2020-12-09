f38127ef4ec34f485235dee815e5e4bd01245c66
240__ <240__@naver.com>
2020-12-09 22:56:32 +0900

    Baekjoon1193

---
+++ src/Baekjoon2/Baekjoon1193.java
@@ -0,0 +1,41 @@
+package Baekjoon2;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Baekjoon1193 {
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int a = 1;
+		int b = 1;
+		int temp = 1;
+		while(num>0) {
+			
+			
+			num--;
+			
+		}
+	}
+}
+/*
+1-1/1
+2-1/2
+3-2/1
+4-3/1
+5-2/2
+6-1/3
+7-1/4
+8-2/3
+9-3/2
+10-4/1
+11-5/1
+12-4/2
+13-3/3
+14-2/4
+15-1/5
+16-1/6
+17-2/5
+18-3/4
+
+*/
