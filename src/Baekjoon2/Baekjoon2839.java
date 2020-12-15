9d280259cfebb6da10f4b4deffa074103ca6e04e
240__ <240__@naver.com>
2020-12-15 22:11:26 +0900

    Baekjoon2839설탕배달

--- src/Baekjoon2/Baekjoon2839.java
+++ src/Baekjoon2/Baekjoon2839.java
@@ -8,33 +8,60 @@ import java.util.Scanner;
  * 18 4 //5 5 5 3
  * 숫자 %5 다 해고 누적해서 %3
  * 15
+ * 
+ * 
+ * 21
+ * 7봉지
+ * 5 5 5 3 3
  */
 public class Baekjoon2839 {
 	public static void main(String[] args) throws IOException{
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
 		int bag = 0;
-		while(num/5>0) {
-			bag = num/5; // 3;
-			num %= 5;
-			System.out.println("num"+num);
-		}
-		if(num/3==0) {
-			bag = -1;
-		}
-		//num== 4면, 
-		
-		while(num/3>0) { //num/3 ==1
-			if(num%3==0) {
-				bag++;
-				num =0;
-			}else {
-				bag = -1;
-				num =0;
+		int temp = num;
+		int three = 0;
+		int five = 0;
+		int mix = 0; //temp=18
+		while(temp>0) {
+			if(temp<=2) {
+				break;
 			}
-			
+			temp -= 3;
+			three++;
 		}
-		
-		System.out.println(bag);
+		if(temp!=0) {
+			three=1000;
+		}
+		temp = num;
+		while(temp>0) {
+			if(temp<=4) {
+				break;
+			}
+			temp -= 5;
+			five++;
+		}
+		if(temp!=0) {
+			five=1000;
+		}
+		temp=num;
+		while(temp>0) { 
+			temp -= 5; 
+			mix++;
+			if(temp%3==0) { //temp==6
+				mix += temp/3; //mix += 2;
+				temp -= 3*(temp/3);
+			}
+		}
+		if(temp!=0) {
+			mix=1000;
+		}
+		int answer =0;
+		if(three==1000&&five==1000&&mix==1000) {
+			answer = -1;
+		}else {
+			answer = Math.min(three, Math.min(five, mix));
+		}
+		System.out.println(answer);
 	}
 }
