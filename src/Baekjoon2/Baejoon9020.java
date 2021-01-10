2f65d37577b9fd818ff48326b12d92ae1f1d9f64
240__ <240__@naver.com>
2021-01-10 23:33:31 +0900

    Baekjoon9020 골드바흐의 추측-1/9~1/10

--- src/Baekjoon2/Baejoon9020.java
+++ src/Baekjoon2/Baejoon9020.java
@@ -1,9 +1,32 @@
 package Baekjoon2;
 
-import java.io.IOException;
-
+import java.util.Scanner;
+//∞ÒµÂπŸ»Â¿« √ﬂ√¯
 public class Baejoon9020 {
-	public static void main(String[] args) throws IOException{
-		
-	}
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int testnum = sc.nextInt();
+        for(int i=0;i<testnum;i++){
+            int a= sc.nextInt();
+            bach(a);
+        }
+        sc.close();
+    }
+    public static void bach(int a){
+        int num=0;
+        for(int i=2;i<=(a/2);i++){ 
+            if(!decimal(i)) continue; 
+            if(decimal(i)&&decimal(a-i)) num=i;
+        }
+        System.out.println(num+" "+(a-num));
+    }
+
+    public static boolean decimal(int a){
+        for(int i=2;i<=Math.sqrt(a);i++){
+            if(a%i==0) return false;
+        }
+    return true;
+
+    }
+
 }
