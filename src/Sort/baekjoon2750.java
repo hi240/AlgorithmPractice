9b3bd0759a6a57085219e8c800aff99d7539df24
240__ <240__@naver.com>
2021-01-28 16:02:10 +0900

    백준 2750 수 정렬하기 - 1/28

---
+++ src/Sort/baekjoon2750.java
@@ -0,0 +1,29 @@
+package Sort;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+//수 정렬하기
+public class baekjoon2750 {
+	public static void main(String[] args) throws IOException{
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int count = Integer.parseInt(br.readLine());
+		int[] arr = new int[count];
+		for(int i=0;i<count;i++) {
+			arr[i]=Integer.parseInt(br.readLine());
+		}
+		br.close();
+		Arrays.sort(arr);
+		for(int i=0;i<count;i++) {
+			bw.write(String.valueOf(arr[i]));
+			bw.newLine();
+		}
+		bw.flush();
+		bw.close();
+	}
+}
