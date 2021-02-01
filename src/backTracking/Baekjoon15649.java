6e3b54c4c3af74888d1d093862307f285d2aa0aa
240__ <240__@naver.com>
2021-02-01 17:23:15 +0900

    백준15649 N과 M(1) - 2/1

---
+++ src/backTracking/Baekjoon15649.java
@@ -0,0 +1,42 @@
+package backTracking;
+
+import java.io.IOException;
+import java.util.Scanner;
+//n°ú m(1)
+public class Baekjoon15649 {
+	static boolean check[];
+	static int arr[];
+	static int index=0;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt(); 
+		sc.close();
+		check = new boolean [n];
+		arr = new int[m];
+		
+		recursive(n,m,0);
+		System.out.println(sb);
+	}
+	public static void recursive (int n,int m,int index) {
+		if(index==m) { 
+			for(int i=0;i<arr.length;i++) {
+				sb.append(arr[i]);
+				if(i!=arr.length-1)sb.append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int i=0;i<n;i++) {
+			if(!check[i]) { 
+				check[i]=true;
+				arr[index]=i+1; 
+				recursive(n,m,index+1);
+				check[i]=false;
+			}
+		}
+		return;
+	}
+	
+}
