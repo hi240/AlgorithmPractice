6bd7f6ac8855ed692fdee70d17920b672dce4b80
hi240 <240__@naver.com>
2021-02-21 01:44:22 +0900

    재귀함수 문제 복습

--- src/backTracking/Baekjoon15649_repeat.java
+++ src/backTracking/Baekjoon15649_repeat.java
@@ -3,7 +3,7 @@ package backTracking;
 import java.io.IOException;
 import java.util.Scanner;
 /*
-4 2 ¸é
+4 4 ¸é
 1 2 3 4
 1 2 4 3
 1 3 2 4
@@ -33,24 +33,32 @@ public class Baekjoon15649_repeat {
 	public static int a;
 	public static int b;
 	public static int[] arr;
+	public static boolean[] visit;
 	public static void main(String[] args) throws IOException{
 		Scanner sc = new Scanner(System.in);
 		a = sc.nextInt(); //4
 		b = sc.nextInt(); //2
 		sc.close();
-		arr= new int[a];
-		recursive(1,0);
+		arr= new int[b];
+		visit=new boolean[a];
+		recursive(0);
 	}
-	public static void recursive(int at,int depth) {
+	public static void recursive(int depth) {
 		if(depth==b) {
-			for(int i=0;i<a;i++) {
+			for(int i=0;i<b;i++) {
 				System.out.print(arr[i]+" ");
 			}
 			System.out.println();
+			return;
 		}
-		for(int i=at;i<=a;i++) {
-			arr[depth]=i;
-			recursive(i,depth+1);
+		for(int i=0;i<a;i++) {
+			if(visit[i]==false) {
+				visit[i]=true;
+				arr[depth]=i+1;
+				recursive(depth+1);
+				visit[i]=false;
+			}
 		}
+		//return;
 	}
 }
--- src/backTracking/Beakjoon15651_repeat.java
+++ src/backTracking/Beakjoon15651_repeat.java
@@ -2,41 +2,30 @@ package backTracking;
 
 import java.io.IOException;
 import java.util.Scanner;
-/* 4 2
-1 1
-1 2
-1 3
-1 4
-2 2
-2 3
-2 4
-3 3
-3 4
-4 4
- */
-public class Baekjoon15652_repeat {
-	static int[] arr ;
-	static int a;
+
+public class Beakjoon15651_repeat {
+	public static int n;
+	public static int m;
+	public static int[] arr;
 	public static void main(String[] args) throws IOException{
 		Scanner sc = new Scanner(System.in);
-		a = sc.nextInt();
-		int b = sc.nextInt();
+		n = sc.nextInt();
+		m = sc.nextInt();
 		sc.close();
-		arr= new int[b];
-		dfs(1,0);
+		arr = new int[m]; 
+		dfs(0,0);
 	}
 	public static void dfs(int at,int depth) {
-		if(arr.length==depth) {
-			for(int i=0;i<arr.length;i++) {
+		if(depth==m) {
+			for(int i=0;i<m;i++) {
 				System.out.print(arr[i]+" ");
 			}
 			System.out.println();
 			return;
 		}
-		for(int i=at;i<=a;i++) {
+		for(int i=1;i<=n;i++) {
 			arr[depth]=i;
-			dfs(at,depth+1);
+			dfs(i,depth+1);
 		}
 	}
 }
-
