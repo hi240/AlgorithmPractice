c2454dc26a8680d0b265f996a126c4474d39db49
hi240 <240__@naver.com>
2021-03-12 20:16:42 +0900

    프로그래머스 보호소에서 중성화한 동물

--- src/MySQL/Programmers59045.java
+++ src/MySQL/Programmers59045.java
@@ -1,5 +1,10 @@
 package MySQL;
-
+//보호소에서 중성화한 동물
 public class Programmers59045 {
-
+//	SELECT B.ANIMAL_ID, B.ANIMAL_TYPE, B.NAME,A.SEX_UPON_INTAKE
+//	FROM ANIMAL_INS A
+//	JOIN ANIMAL_OUTS B
+//	ON A.ANIMAL_ID = B.ANIMAL_ID
+//	WHERE (A.SEX_UPON_INTAKE like 'Spayed Female')
+//	ORDER BY ANIMAL_ID;
 }
