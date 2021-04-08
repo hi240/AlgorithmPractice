cca267a8bb493f8667f46418315fe5c984ea3f64
hi240 <240__@naver.com>
2021-04-08 18:36:38 +0900

    백준10830 행렬 제곱

--- src/DivideConquer/Baekjoon10830.java
+++ src/DivideConquer/Baekjoon10830.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
-
+//Çà·Ä Á¦°ö
 public class Baekjoon10830 {
 	public static long[][] arr;
 	public static int n;
