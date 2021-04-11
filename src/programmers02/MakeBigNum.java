3b77a99e4c6dfd216489cd7269b410a9e2eea090
hi240 <240__@naver.com>
2021-04-12 03:12:52 +0900

    프로그래머스 큰 수 만들기 진행중

---
+++ src/programmers02/MakeBigNum.java
@@ -0,0 +1,14 @@
+package programmers02;
+//큰 수 만들기
+public class MakeBigNum {
+
+	public static void main(String[] args) {
+		String str= "40122";
+		char[] ch = str.toCharArray();
+		String s = String.valueOf(ch[0]);
+		int a = ch[0];
+		System.out.println(s+" "+a);
+		Integer.parse
+	}
+
+}
