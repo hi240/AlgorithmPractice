2d6fbe4d081a6635729b84d7d2478ed2bcfa2e9a
hi240 <240__@naver.com>
2021-04-13 17:49:22 +0900

    프로그래머스 큰 수 만들기

--- src/programmers02/MakeBigNum.java
+++ src/programmers02/MakeBigNum.java
@@ -1,14 +1,28 @@
 package programmers02;
 //큰 수 만들기
 public class MakeBigNum {
-
 	public static void main(String[] args) {
-		String str= "40122";
-		char[] ch = str.toCharArray();
-		String s = String.valueOf(ch[0]);
-		int a = ch[0];
-		System.out.println(s+" "+a);
-		Integer.parse
+		String str="4177252841";
+		int k=4;
+		System.out.println(solution(str,k));
 	}
-
+	public static String solution(String number, int k) {
+        String answer = "";
+        StringBuffer sb = new StringBuffer();
+        int pop=0;
+        char max;
+        for(int i=0;i<number.length()-k;i++) {
+        	max='0';
+        	for(int j=pop;j<=k+i;j++) {
+        		System.out.println(j);
+        		if(number.charAt(j)>max) {
+        			max=number.charAt(j);
+        			pop = j+1;
+        		}
+        	}
+        	sb.append(max);
+        }
+        answer=String.valueOf(sb);
+        return answer;
+    }
 }
