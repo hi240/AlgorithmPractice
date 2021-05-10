49f80b0f4f2e20ed481c31462b8e3ddd8a7efd6f
hi240 <240__@naver.com>
2021-05-11 00:00:54 +0900

    백준10773 스택

---
+++ src/Stack/baekjoon10773.java
@@ -0,0 +1,23 @@
+package Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+//Á¦·Î
+public class baekjoon10773 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		Stack<Integer> stk = new Stack<Integer>();
+		for(int i=0;i<num;i++) {
+			int n=sc.nextInt();
+			if(n==0) stk.pop();
+			stk.add(n);
+		}
+		int sum=0;
+		while(!stk.isEmpty()) {
+			sum +=stk.pop();
+		}
+		System.out.println(sum);
+	}
+}
