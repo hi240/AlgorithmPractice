f5707375ebe2ae1bcbeccbfbdc6253a9c1b95194
hi240 <240__@naver.com>
2021-08-03 18:33:53 +0900

    백준10816 숫자 카드 2

--- FastCampus/search/BOJ10816.java
+++ FastCampus/search/BOJ10816.java
@@ -11,28 +11,24 @@ public class BOJ10816 {
         sol();
     }
     public static void sol(){
-        Arrays.sort(narr);
+        Arrays.sort(narr,1,N+1);
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<M;i++){
-           int upper=upper_bound(1,narr.length-1,marr[i]);
-            System.out.println(upper+" u");
-           int lower=lower_bound(1,narr.length-1,marr[i]);
-            System.out.println(lower+" l");
-            int num= upper-lower;
-           if(upper!=0&&lower!=0) num+=1;
-           sb.append(num).append(" ");
+            sb.append(upper_bound(1,N,marr[i] )-lower_bound(1,N,marr[i]));
+            sb.append(" ");
         }
-        System.out.println(sb.toString());
+        sb.append("\n");
+        System.out.print(sb.toString());
     }
-    public static int lower_bound(int l,int r,int x){ //marr[i]랑 같은값 중에 젤 왼쪽인덱스스
+    public static int lower_bound(int l,int r,int x){ //marr[i]랑 같은값 중에 젤 왼쪽인덱스
         int res=r+1;
         while(l<=r){
             int mid=(l+r)/2;
-            if(narr[mid]<x){
-                l=mid+1;
-            }else{
-                if(narr[mid]==x)res=mid;
+            if(narr[mid]>=x){
+                res=mid;
                 r=mid-1;
+            }else{
+                l=mid+1;
             }
         }
         return res;
@@ -41,11 +37,11 @@ public class BOJ10816 {
         int res=r+1;
         while(l<=r){
             int mid=(l+r)/2;
-            if(narr[mid]<=x){
-                if(narr[mid]==x)res=mid;
-                l=mid+1;
-            }else{
+            if(narr[mid]>x){
+                res=mid;
                 r=mid-1;
+            }else{
+                l=mid+1;
             }
         }
         return res;
