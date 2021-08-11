c5cb9e34bd36188678729f25de086a4cb0941149
hi240 <240__@naver.com>
2021-08-11 21:04:23 +0900

    백준1806 부분합

--- FastCampus/twoPointers/BOJ1806.java
+++ FastCampus/twoPointers/BOJ1806.java
@@ -1,7 +1,7 @@
 package twoPointers;
 
 import java.util.Scanner;
-
+//부분합
 public class BOJ1806 {
     public static int N,S;
     public static int[] arr;
@@ -10,27 +10,28 @@ public class BOJ1806 {
         sol();
     }
     public static void sol(){
-        int l=0;
         int r=0;
         int sum=0;
         int min=Integer.MAX_VALUE;
-        while(l>r){
-            r++;
-            for(int i=l;i<r;i++){
-                sum += arr[i];
+        for(int l=1;l<=N;l++){
+            sum -= arr[l-1]; //for문을 돌았다는 것은, l이 줄어들 차례가 왔다는 의미
+            while(sum<S && r<N){ //r의 범위가 배열을 넘지 않게 조심
+                sum += arr[++r]; //S를 넘을 때까지 r의 범위를 늘려서 누적
             }
-            if(sum>=S){
-                min=Math.min(min,r-l+1);
-                l++;
-            }
+            if(sum>=S)
+                min=Math.min(min,r-l+1); //최소 길이 갱신
         }
+        if(min==Integer.MAX_VALUE)
+            System.out.println("0");
+        else
+            System.out.println(min);
     }
     public static void input() {
         Scanner sc = new Scanner(System.in);
         N=sc.nextInt();
         S=sc.nextInt();
-        arr = new int[N];
-        for(int i=0;i<N;i++){
+        arr = new int[N+1];
+        for(int i=1;i<=N;i++){
             arr[i]=sc.nextInt();
         }
     }
