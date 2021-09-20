105f2e72d0d5c6f96d68ef1669ab30463abe29d3
hi240 <240__@naver.com>
2021-09-21 02:00:12 +0900

    백준5639 이진 검색 트리 진행중

--- FastCampus/graphSearch/BOJ5639.java
+++ FastCampus/graphSearch/BOJ5639.java
@@ -1,44 +1,46 @@
 package graphSearch;
 
+import com.sun.javafx.image.IntPixelGetter;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 //이진 검색 트리
 public class BOJ5639 {
-    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static int N,M;
-    public static int[] graph;
-    public static ArrayList<Integer>[] list;
+    public static ArrayList<Integer> list;
+    public static int ind;
+    public static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         input();
-        sol();
+        dfs(0,0); //후위순회
+        System.out.println(sb.toString());
     }
-    public static void sol(){
+    public static void dfs(int index,int mom){ //4,3
+        if(list.get(index)>list.get(mom)){
+            sb.append(list.get(mom)).append('\n').append(list.get(index)).append('\n'); //28
+            ind=index+1;
+            return;
+        }
+        dfs(index+1,index);
 
+        sb.append(list.get(mom)).append('\n');
+        if(index==50) return;
     }
     public static void input() throws IOException {
-        String[] st = br.readLine().split(" ");
-        N=Integer.parseInt(st[0]);
-        M=Integer.parseInt(st[1]);
-
-        //그래프
-        graph = new int[N+1];
-        String[] str =br.readLine().split(" ");
-        for(int i=1;i<=N;i++){
-            graph[i]=Integer.parseInt(str[i-1]);
+        list = new ArrayList<>(); //50 30 24 5 28 45 98 52 60
+        Scanner sc =new Scanner(System.in);
+        while(sc.hasNextLine()){
+            String str=sc.nextLine();
+            if(str.equals("")) break;
+            list.add(Integer.parseInt(str));
         }
-
-        //정점
-        list = new ArrayList[N+1];
-        for(int i=0;i<list.length;i++){
-            list[i]= new ArrayList<Integer>();
-        }
-        for(int i=0;i<M;i++){
-            String[] s = br.readLine().split(" ");
-            int x=Integer.parseInt(s[0]);
-            int y=Integer.parseInt(s[1]);
-        }
+        sc.close();
+//        tree= new ArrayList[1000001];
+//        for(int i=0;i<tree.length;i++){
+//            tree[i]=new ArrayList<>();
+//        }
     }
 }
