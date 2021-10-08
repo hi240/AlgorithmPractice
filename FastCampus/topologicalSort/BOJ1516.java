cca9e03d2d0c1ca1a364aad1b94a94337c53dd96
hi240 <240__@naver.com>
2021-10-08 21:31:09 +0900

    백준1516 게임 개발 진행중

---
+++ FastCampus/topologicalSort/BOJ1516.java
@@ -0,0 +1,25 @@
+package topologicalSort;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+//20211008
+//게임 개발
+public class BOJ1516 {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static ArrayList<Integer>[] list;
+    public static int[] indeg;
+    public static int n,m,k;
+    public static void main(String[] args) throws IOException {
+        input();
+    }
+    public static void input() throws IOException {
+        n=Integer.parseInt(br.readLine());
+        for(int i=0;i<n;i++){
+            String[] str=br.readLine().split(" ");
+
+
+        }
+    }
+}
