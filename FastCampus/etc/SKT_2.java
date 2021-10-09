70106bc292caab49708865ea917489f104973f20
hi240 <240__@naver.com>
2021-10-09 12:52:35 +0900

    2번째 문제 복기 [완전탐색]

--- FastCampus/etc/SKT_2.java
+++ FastCampus/etc/SKT_2.java
@@ -4,10 +4,9 @@ import java.io.IOException;
 //완전탐색
 /*
 a b c d e 의 키 값이 주어졌을 떄, |a-b|+|b-c|+|c-d|+|d-e|의 값이 최대값이 되게 abcde의 순서를 바꿔 그 최대값을 출력하세요.
-아직도 미완성
  */
 public class SKT_2 {
-    static int[] arr={20,8,10,1,4,15};
+    static int[] arr={20,8,10,1,4,15}; //62
     static int max;
     public static void main(String[] args) throws IOException {
         max=0;
@@ -26,7 +25,6 @@ public class SKT_2 {
         for(int i=0;i<visit.length;i++){
             if(visit[i]) continue;
             visit[i]=true;
-            System.out.println(prev+" "+arr[i]+" "+sum+" "+cnt);
             dfs(arr[i],sum+Math.abs(prev-arr[i]),cnt+1,visit);
             visit[i]=false;
         }
