ad7ef3854394309102a7c416c440603b05c25600
hi240 <240__@naver.com>
2021-10-14 18:17:50 +0900

    백준2011 -암호코드 진행중

---
+++ FastCampus/dynamicProgramming/BOJ2011.java
@@ -0,0 +1,14 @@
+package dynamicProgramming;
+
+import java.io.IOException;
+
+//20211014
+//암호코드
+public class BOJ2011 {
+    public static void main(String[] args) throws IOException {
+        input();
+    }
+    public static void input() throws IOException {
+
+    }
+}
