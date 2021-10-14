ab8c6b24a07bf0ad42ae152b29017f1076d1096a
hi240 <240__@naver.com>
2021-10-14 15:11:44 +0900

    백준15991 1,2,3 더하기 6 -다이나믹프로그래밍

--- FastCampus/dynamicProgramming/BOJ15991.java
+++ FastCampus/dynamicProgramming/BOJ15991.java
@@ -7,10 +7,18 @@ import java.io.InputStreamReader;
 //1,2,3 더하기 6
 //n을 1,2,3의 덧셈으로 만들되, 연산이 과정이 대칭이어야함
 public class BOJ15991 {
-    public static int[] dp;
+    public static long[] dp;
     public static void sol() {
-        dp=new int[1000001];
-
+        dp=new long[100001];
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=2;
+        dp[4]=3;
+        dp[5]=3;
+        dp[6]=6;
+        for(int i=7;i<100001;i++){
+            dp[i]=(dp[i-2]+dp[i-4]+dp[i-6])%1000000009;
+        }
     }
     public static void input() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
