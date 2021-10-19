b2eab85e68ba333c7e9a5ab5ccfe7f2cbd3a658e
hi240 <240__@naver.com>
2021-10-19 17:13:35 +0900

    진행중 임시저장

---
+++ FastCampus/dynamicProgramming/BOJ1149.java
@@ -0,0 +1,16 @@
+package dynamicProgramming;
+
+import java.io.IOException;
+
+//RGB거리
+public class BOJ1149 {
+    public static void main(String[] args) throws IOException {
+        input();
+    }
+    public static void input() throws IOException {
+
+    }
+    public static void pro() {
+
+    }
+}
