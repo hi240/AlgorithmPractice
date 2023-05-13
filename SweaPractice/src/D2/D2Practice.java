25c34d86cc796d66b68041f07469833c9659c723
240__ <240__@seoul>
2023-05-13 13:11:46 +0900

    test

--- SweaPractice/src/D2/D2Practice.java
+++ SweaPractice/src/D2/D2Practice.java
@@ -1,7 +1,37 @@
 package D2;
 
+import java.util.HashSet;
+import java.util.TreeSet;
+
 public class D2Practice {
 	public static void main(String[] args){
-		System.out.println("hello eclipse again");
+
+		HashSet<Integer> hash = new HashSet<>();
+        hash.add(2);
+        hash.add(3);
+        hash.add(1);
+        hash.add(4);
+        hash.remove(4);
+        
+        for(Integer i : hash){
+        	System.out.print(i);
+        }
+        System.out.println();
+       
+        TreeSet<Integer> tree = new TreeSet<>();
+        tree.add(1);
+        tree.add(4);
+        tree.add(3);
+        tree.add(17);
+
+        for(Integer i : tree){
+        	System.out.print(i);
+        }
+        System.out.println();
+        tree.remove(4);
+        for(Integer i : tree){
+        	System.out.print(i);
+        }
+        System.out.println();    
 	}
 }
